ec5b3307d105f1624e14f3f04f3d1e8d1f5e344f
t0406peru <thomasdastrup@gmail.com>
2021-05-28 18:18:49 -0600

    Update Installer.ps1

--- Installer.ps1
+++ Installer.ps1
@@ -1,6 +1,13 @@
+# Install Server-Media-Foundation without a reboot
 Get-ChildItem "$Env:SystemRoot\Servicing\Packages\*Media*.mum" | ForEach-Object { (Get-Content $_) -replace 'required','no' | Set-Content $_}
 Install-WindowsFeature Server-Media-Foundation
-(New-Object Net.WebClient).DownloadFile('https://downloads.plex.tv/plex-media-server/1.12.3.4973-215c28d86/Plex-Media-Server-1.12.3.4973-215c28d86.exe', '\plex.exe')
+#
+# Updated download link
+(New-Object Net.WebClient).DownloadFile('https://downloads.plex.tv/plex-media-server-new/1.23.2.4600-f06057b33/windows/PlexMediaServer-1.23.2.4600-f06057b33-x86.exe', '\plex.exe')
+#
+# Install plex
 .\plex.exe /quiet
+#
+# Delete installer
 del .\plex.exe
 powershell Clear-RecycleBin
